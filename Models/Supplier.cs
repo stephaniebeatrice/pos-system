82ee23ffdebac63d26470647af06e2b90d9a59b6
stephaniebeatrice <beatriceladida@gmail.com>
2024-02-15 19:50:23 +0300

    dotnet CRUD

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -5,17 +5,17 @@ namespace pos_system.Models
 {
     public class Supplier
 {
-    public int Id  { get; set; };
+    public int Id  { get; set; }
 
-    public string supplierName  { get; set; };
+    public string supplierName  { get; set; }
 
-    public string companyName  { get; set; };
+    public string companyName  { get; set; }
 
-    public string email  { get; set; };
+    public string email  { get; set; }
 
-    public int phoneNumber  { get; set; };
+    public int phoneNumber  { get; set; }
 
-    public string address  { get; set; };
+    public string address  { get; set; }
 
 }
 }
